9291423f4dd76e11df20b0e1ff7d4db2e8288b04
Snehalgaikwad2327 <108270430+Snehalgaikwad2327@users.noreply.github.com>
2022-07-19 13:45:06 +0530

    login page
    with background image

--- main.dart
+++ main.dart
@@ -1,65 +1,109 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
-//import 'image_banner.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ColorChanger(),
+      home: LoginPage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
 
-class ColorChanger extends StatefulWidget {
-  const ColorChanger({Key? key}) : super(key: key);
-
+class LoginPage extends StatefulWidget {
   @override
-  State<ColorChanger> createState() => _ColorChangerState();
+  State<LoginPage> createState() => LoginPageState();
 }
 
-class _ColorChangerState extends State<ColorChanger> {
-  var isred = false;
+class LoginPageState extends State<LoginPage> {
+  var isyellow = true;
   @override
   Widget build(BuildContext context) {
-    final color = isred ? Colors.red : Colors.green;
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Icon Color Changer'),
+    final color = isyellow ? Colors.yellow : Colors.red;
+    // ignore: unnecessary_new
+    return new Scaffold(
+      backgroundColor: Colors.black,
+      // ignore: unnecessary_new
+      body: new Stack(fit: StackFit.expand, children: <Widget>[
+        // ignore: prefer_const_constructors
+        Image(
+          image: AssetImage('assets/images/moonimg.jpg'),
+          color: Colors.black87,
+          colorBlendMode: BlendMode.darken,
         ),
-        body: Center(
-          child: Column(mainAxisSize: MainAxisSize.min, children: [
-            // ignore: prefer_const_constructors
-            Icon(
-              Icons.rectangle_rounded,
-              color: color,
-              size: 100,
+        Form(
+          child: Theme(
+            data: ThemeData(
+                brightness: Brightness.dark,
+                //primarySwatch: Colors.teal,
+                inputDecorationTheme: InputDecorationTheme(
+                    labelStyle: TextStyle(color: Colors.teal, fontSize: 20.0))),
+            child: Container(
+              padding: EdgeInsets.all(50.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.login,
+                      size: 80.0,
+                      color: color,
+                    ),
+                    // Text(
+                    //   "LOGIN PAGE",
+                    //   style: TextStyle(
+                    //     color: Colors.yellowAccent,
+                    //     fontSize: 30.0,
+                    //     fontWeight: FontWeight.bold,
+                    //   ),
+                    // ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Enter Email",
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    TextFormField(
+                      // ignore: prefer_const_constructors
+                      decoration: InputDecoration(
+                        labelText: "Enter Password",
+                      ),
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                    ),
+                    Padding(padding: EdgeInsets.only(bottom: 30.0)),
+                    MaterialButton(
+                      height: 50.0,
+                      minWidth: 100,
+                      color: Colors.teal,
+                      textColor: Colors.black,
+                      child: Text(
+                        "LOGIN",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20.0),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          if (isyellow == true) {
+                            isyellow = false;
+                          } else {
+                            isyellow = true;
+                          }
+                        });
+                      },
+                    )
+                  ]),
             ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  if (isred == true) {
-                    isred = false;
-                  } else {
-                    isred = true;
-                  }
-                });
-              },
-              child: const Text('Click Button'),
-            ),
-            const Image(
-              image: AssetImage('assets/images/zazapro.jpeg'),
-              height: 300.0,
-              width: 100.0,
-            ),
-          ]),
-        ));
+          ),
+        )
+      ]),
+    );
   }
 }
